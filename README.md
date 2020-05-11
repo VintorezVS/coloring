c3b02691a9e06aaf4070b8f93d1c3e432e2d7d72
Vintorez <vintorezvs@gmail.com>
2020-05-11 18:52:06 +0300

    Implement optimal options for tracing, collect pallet, add outline and add some code for writing color numbers

---
+++ README.md
@@ -0,0 +1,55 @@
+To fix error in `svgdom` replace function on line `node_modules/svgdom/main-require.cjs:4558` with following
+
+```
+const pointAtLength = function (d, len) {
+  const segs = pathParser(d)
+
+  const segLengths = segs.map(el => el.length())
+
+  const length = segLengths.reduce((l, c) => l + c, 0)
+
+  let i = 0
+
+  let t = len / length
+
+  // FIXME: Pop Move before using shortcut?
+  // shortcut for trivial cases
+  if (t >= 1) {
+    // Check if there is a p2. If not, use p1
+    if (segs[segs.length - 1].p2) {
+      return segs[segs.length - 1].p2.native()
+    } else {
+      return segs[segs.length - 1].p1.native()
+    }
+  }
+
+  if (t <= 0) return segs[0].p1.native()
+
+  // remove move commands at the very end of the path
+  while (segs[segs.length - 1] instanceof Move) segs.pop()
+
+  let segEnd = 0
+
+  for (let il = segLengths.length; i < il; ++i) {
+    const k = segLengths[i] / length
+    segEnd += k
+
+    if (segEnd > t) {
+      break
+    }
+  }
+
+  if (segLengths.length === i) {
+    if (segs[segs.length - 1].p2) {
+      return segs[segs.length - 1].p2.native()
+    } else {
+      return segs[segs.length - 1].p1.native()
+    }
+  }
+
+  const ratio = length / segLengths[i]
+  t = ratio * (t - segEnd) + 1
+
+  return segs[i].pointAt(t).native()
+}
+```
--- src/index.js
+++ src/index.js
@@ -1,67 +1,48 @@
 const fs = require("fs");
 const { bitmap2vector } = require("bitmap2vector");
-const { createCanvas, loadImage } = require("canvas");
-const PixelMatrix = require("./PixelMatrix");
+const { createSVGWindow } = require('svgdom');
+const window = createSVGWindow();
+const document = window.document;
+const { SVG, registerWindow } = require('@svgdotjs/svg.js');
+require('@svgdotjs/svg.topoly.js');
 
+registerWindow(window, document);
 
-function writeCanvasAsFile(canvas, fileName) {
-    return new Promise((resolve, reject) => {
-        const fd = fs.openSync(`./images/${fileName}`, 'w');
-
-        canvas.toBuffer((bufferError, buffer) => {
-            if (bufferError) {
-                fs.closeSync(fd);
-                reject(bufferError)
-            }
-            fs.write(fd, buffer, (writeError) => {
-                if (writeError) {
-                    reject(writeError);
-                }
-                fs.closeSync(fd);
-                resolve();
-            });
-        });
-    });
-}
-
-// (async function() {
-//     const image = await loadImage('./images/simple.png');
-//     const height = image.height;
-//     const width = image.width;
-//     const canvas = createCanvas(width, height);
-//     const ctx = canvas.getContext('2d');
-//
-//     ctx.drawImage(image, 0, 0, width, height);
-//
-//     const canvasImageData = ctx.getImageData(0, 0, width, height);
-//     const pixelMatrix = new PixelMatrix(canvasImageData);
-//
-//     pixelMatrix.addOutline();
-//
-//     ctx.putImageData(pixelMatrix.toImageData(), 0, 0);
-//
-//     const error = await writeCanvasAsFile(canvas, 'simple-copy.png');
-//
-//     if (error) {
-//         console.error(error);
-//     }
-// })();
-
-fs.readFile('./images/1.jpg', (err, input) => {
-    bitmap2vector({
-        input,
-        // colorsampling: 0,
-        // pal: [{"a":255,"r":255,"g":255,"b":255},{"a":255,"r":245,"g":212,"b":5},{"a":255,"r":36,"g":41,"b":32},{"a":255,"r":237,"g":167,"b":193},{"a":255,"r":140,"g":92,"b":79},{"a":255,"r":4,"g":147,"b":62},{"a":255,"r":5,"g":82,"b":147},{"a":255,"r":218,"g":36,"b":28},{"a":255,"r":16,"g":138,"b":207},{"a":255,"r":171,"g":172,"b":156},{"a":255,"r":204,"g":36,"b":44}],
-        // help: true,
-        // blurradius: 1,
-        // colorsampling: 0,
-        // numberofcolors: 11,
+(async function() {
+    const file = fs.readFileSync('./images/1.jpg');
+    const svgOutput = await bitmap2vector({
+        input: file,
         strokewidth: 0,
-        blurradius:5, blurdelta: 64,
-        ltres:0.01, linefilter:true, rightangleenhance:false,
-        colorsampling:0, numberofcolors:27
-        // pathomit: 4,
-    }).then((out) => {
-        fs.writeFileSync('./images/output2.svg', out.content);
-    })
-});
+        blurradius:3, blurdelta: 256,
+        ltres:0.01, qtres:0.01, linefilter:true, rightangleenhance:false,
+        colorsampling:0, numberofcolors:27,
+        pathomit: 6, roundcoords:2,
+    });
+    let svgString = svgOutput.content;
+
+    const strokeWidthRegex = /stroke-width=".+?"/g;
+    const strokeRegex = /stroke=".+?"/g;
+    const fillRegexString = 'fill="(.+?)"';
+    const fillRegexLocal = new RegExp(fillRegexString);
+    const fillRegexGlobal = new RegExp(fillRegexString, 'g');
+    const colors = svgString.match(fillRegexGlobal).map((fillString) => fillString.match(fillRegexLocal)[1])
+        .reduce((acc, colorString) => acc.includes(colorString) ? acc : [...acc, colorString], []);
+
+    console.log(`COLORS (${colors.length}): `, colors);
+
+    svgString = svgString.replace(strokeWidthRegex, 'stroke-width="0.25"');
+    svgString = svgString.replace(strokeRegex, 'stroke="rgb(127,127,127)"');
+    svgString = svgString.replace(fillRegexGlobal, 'fill="none"');
+
+    // const canvas = SVG(document.documentElement);
+    // const svgContent = svgString.replace(/<svg.+?>/, '<g id="polygons">').replace('</svg>', '</g>');
+
+    // canvas.svg(svgContent).size(svgOutput.width, svgOutput.height);
+    // canvas.findOne('#polygons').children().forEach((element) => {
+        // todo: using polygons' points and Google search results of "Алгоритм определения попадания точки в контур"
+        //       find good position for writing color number there
+        // element.array().toPoly();
+    // });
+
+    fs.writeFileSync('./images/output.svg', svgString);
+})();
